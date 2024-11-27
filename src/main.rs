808d8523356c88875fefc2abfe94b6e26be79268
Tim Ritmeester <timritmeester@gmail.com>
2024-11-27 21:49:58 +0100

    read file to polars dataframe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use polars::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut df: DataFrame = CsvReadOptions::default()
+        .with_has_header(false).try_into_reader_with_file_path(Some("input_1.txt".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+
+    println!("{:?}", df);
