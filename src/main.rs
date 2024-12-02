11c4231b273bc064225093e7da18d7644d4bac83
Tim Ritmeester <timritmeester@gmail.com>
2024-12-02 10:27:23 +0100

    Working version with polars

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use polars::prelude::*;
 
 fn main() {
     let mut df: DataFrame = CsvReadOptions::default()
-        .with_has_header(false).try_into_reader_with_file_path(Some("input_1.txt".into()))
+        .with_has_header(false).try_into_reader_with_file_path(Some("../input_advent/input_1_dev.txt".into()))
         .unwrap()
         .finish()
         .unwrap();
-
     println!("{:?}", df);
+    println!("{:?}", df);}
