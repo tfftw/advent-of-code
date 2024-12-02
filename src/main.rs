4a8207b9658a5d16591086f95feb0f9d4b74e673
Tim Ritmeester <timritmeester@gmail.com>
2024-12-02 11:33:48 +0100

    Implement csv to read lines

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use polars::prelude::*;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+use csv::StringRecord;
 
-fn main() {
-    let mut df: DataFrame = CsvReadOptions::default()
-        .with_has_header(false).try_into_reader_with_file_path(Some("../input_advent/input_1_dev.txt".into()))
-        .unwrap()
-        .finish()
-        .unwrap();
-    println!("{:?}", df);
-    println!("{:?}", df);}
+
+
+fn read_csv<P: AsRef<Path>>(filename: P) -> Result<Vec<String>, Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    let mut numbers = Vec::new();
+
+    for result in rdr.records() {
+        let record = result?;
+        numbers.push((StringRecord::as_slice(&record).to_owned()))
+    }
+
+    Ok((numbers))
+}
+
+
+
+fn main() -> Result<(), Box<dyn Error>>  {
+    let filename = "../input_advent/input_1_dev.txt";
+    let lines = read_csv(filename);
+
+    for line in lines {
+        println!("{:?}", line)
+    }
+    Ok(())
+}
